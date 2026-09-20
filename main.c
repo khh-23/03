1e3ae53de84201a7a75cabefc22b1c444be77a6d
khh-23 <kkhn@sookmyung.ac.kr>
2026-09-21 01:17:41 +0900

    assignment4

--- main.c
+++ main.c
@@ -1,14 +1,18 @@
 #include <stdio.h>
 
 int main(void) {
-    char c;
-    
-    printf("enter a character : ");
-    scanf("%c", &c);
+    int a, b;
+    float result;
 
-    c = c + 1;
+    printf("분자를 입력하세요 : ");
+    scanf("%d", &a);
 
-    printf("the next character of %c (%i) is %c (%i)\n", c -1, c - 1, c, c);
+    printf("분모를 입력하세요 : ");
+    scanf("%d", &b);
+
+    result = (float)a / b;
+
+    printf("나누기의 결과는 %f 입니다.\n", result);
 
     return 0;
 }
